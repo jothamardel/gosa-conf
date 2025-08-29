9253a301b9e02651d74061c74fe7e1d2bd30154c
jothamardel <ardelmbiplang@gmail.com>
2025-08-29 13:00:13 +0100

    chore: updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- components/forms/convention-brochure.tsx
+++ components/forms/convention-brochure.tsx
@@ -26,8 +26,8 @@ interface Errors {
 }
 
 const BROCHURE_PRICING = {
-  digital: +process.env.BROCHURE_PHYSICAL_FEE as number,
-  physical: +process.env.BROCHURE_PHYSICAL_FEE as number
+  digital: process.env.BROCHURE_PHYSICAL_FEE ||2200,
+  physical: process.env.BROCHURE_PHYSICAL_FEE ||1200
 }
 // BROCHURE_PHYSICAL_FEE=1200
 // BROCHURE_DIGITAL_FEE=2200
